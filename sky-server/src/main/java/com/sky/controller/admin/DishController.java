1f7800f66c9f6e905cdee0dbbdcd098391c215e2
liudingyu <2738961616@qq.com>
2024-02-20 20:04:01 +0800

    缓存菜品功能

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -10,9 +10,12 @@ import com.sky.service.DishService;
 import com.sky.vo.DishVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("admin/dish")
@@ -23,10 +26,16 @@ import java.util.List;
 public class DishController {
     @Autowired
     private DishService dishService;
+    @Autowired
+    private RedisTemplate redisTemplate;
     @PostMapping()
     public Result save(@RequestBody DishDTO dishDTO){
         log.info("新增菜品 {}",dishDTO);
         dishService.saveWithFlavor(dishDTO);
+
+        //清除该菜品所属分类的缓存
+        String key = "dish_" + dishDTO.getCategoryId();
+        cleanCache(key);
         return Result.success();
     }
 
@@ -51,6 +60,10 @@ public class DishController {
     public Result delete(@RequestParam List<Long> ids){
         log.info("菜品删除 {}",ids);
         dishService.deleteBatch(ids);
+
+        //批量删除菜品时，菜品可能对应一个分类也可能对应多个分类，想知道对应的分类还需要查询。为简化操作直接删除所有缓存
+        //删除所有key为dish_*格式的缓存
+        cleanCache("dish_*");
         return Result.success();
     }
 
@@ -76,6 +89,11 @@ public class DishController {
     public Result update(@RequestBody DishDTO dishDTO){
         log.info("修改菜品 {}",dishDTO);
         dishService.updateWithFlavor(dishDTO);
+
+        //修改菜品时可能会修改菜品的分类，会影响redis中的两条记录，简便起见直接删除所有分类-菜品的缓存
+        //删除所有key为dish_*格式的缓存
+        cleanCache("dish_*");
+
         return Result.success();
     }
 
@@ -100,7 +118,19 @@ public class DishController {
     public Result updateStatus(@PathVariable int status,Long id){
         log.info("修改起售停售信息,状态:{} id:{}",status,id);
         dishService.updateStatus(status,id);
+
+        //删除所有key为dish_*格式的缓存
+        cleanCache("dish_*");
         return Result.success();
+    }
+
+    /**
+     * 统一清理redis中的缓存
+     * @param pattern 需要清理的redis的key的模式
+     */
+    private void cleanCache(String pattern){
+        Set keys = redisTemplate.keys(pattern);
+        redisTemplate.delete(keys);
     }
 
 }
